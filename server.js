13ea44ee466076ddd804b97f82a3a714721bc6f8
DillonKyleDev <dillonkyle95@gmail.com>
2021-12-28 14:54:59 -0800

    staging for deployment

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ const DeleteUserRouter = require('./Routes/DeleteUser');
 
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
-app.use('/static', express.static(path.resolve(__dirname, "./Client/build")));
+app.use(express.static(path.resolve(__dirname, "./Client/build")));
 
 //Connect MongoDB
 const mongoose = require('mongoose');
